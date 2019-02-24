75bb836cfbbd533b26c142d038f4e6ce5e81f51d
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-02-24 23:21:38 +0900

    アカウント発行のログファイルのPATHを変更

--- lib/Define.php
+++ lib/Define.php
@@ -1,7 +1,7 @@
 <?php 
 define('PATH_ACCOUNT_JSON', './account.json');
 define('PATH_USERNAMES_EXAMPLE', './storage/usernames_example.txt');
-define('PATH_GENERATED_ACCOUNT', './generated_account.txt');
+define('PATH_GENERATED_ACCOUNT', './log/generated_account.txt');
 
 define('ERROR_FETAIL_GET_AUTH_CODE', '認証処理で失敗しました。ウィンドウサイズを大きくするなど調整してみてください。');
 define('ERROR_RETRY_AUTH', '認証コードの取得に失敗しました。リトライします。');
